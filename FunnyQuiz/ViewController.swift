a308346b23e6b2f84ebf6e453021d6f44981be9e
Trung iOS <trungnt@vps.com.vn>
2020-06-07 21:01:43 +0700

    Initial Commit

---
+++ FunnyQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FunnyQuiz
+//
+//  Created by Trung iOS on 6/7/20.
+//  Copyright © 2020 Fighting. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
